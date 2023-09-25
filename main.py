7ce5a0d7984bd2d75e98368c84c51c3d4340d5b9
DerenB <dbozerprogramming@gmail.com>
2023-09-25 00:31:12 -0400

    Updating CORS

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ from DatabaseFunctions import (
 origins = [
     'http://localhost:3000',
     'https://localhost:3000',
-    'http://127.0.0.1:8000/'
+    'http://127.0.0.1:8000/',
+    'https://protected-meadow-39795-d8c32d547435.herokuapp.com/'
 ]
 
 # Middleware
